59ba2e6d400a256e7d7e902c2866f96936441b22
learning1234embed <38193520+learning1234embed@users.noreply.github.com>
2018-04-11 21:59:49 -0400

    Add files via upload

--- intermittent_learning/intermittent_learning.c
+++ intermittent_learning/intermittent_learning.c
@@ -1,6 +1,7 @@
 #include <msp430.h>
 #include <stdio.h>
 #include "driverlib.h"
+#include "memory_util.h"
 #include "actions.h"
 
 #define NUM_ACTION              (11)
@@ -32,48 +33,6 @@ float learn_infer_ratio = 0;
 #pragma PERSISTENT(learning_performance)
 int learning_performance = 0;
 
-
-long lr(long *addr)
-{
-    return __data20_read_long((unsigned long int)addr);
-}
-
-void lw(long *addr, long i)
-{
-    __data20_write_short((unsigned long int)addr, i);
-}
-
-int ir(int *addr)
-{
-    int temp = __data20_read_short((unsigned long int)addr);
-    int *i = (int *)&temp;
-    return *i;
-}
-
-void iw(int *addr, int f)
-{
-    int *i = (int *)&f;
-    __data20_write_short((unsigned long int)addr, *i);
-}
-
-float fr(float *addr)
-{
-    unsigned long temp = __data20_read_long((unsigned long int)addr);
-    float *f = (float *)&temp;
-    return *f;
-}
-
-void fw(float *addr, float f)
-{
-    uint32_t *i = (uint32_t *)&f;
-    __data20_write_long((unsigned long int)addr, *i);
-}
-
-int execute(long action_address)
-{
-    int (*action)() = (int (*)())action_address;
-    return (*action)();
-}
 
 int (*state_diagram())()
 {
